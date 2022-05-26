a76119d7e921e45b1ad17ed652f8db1d926e2e92
Ryan Miller <ryan.miller@rsmiller.net>
2022-05-26 13:46:27 -0600

    determines possible species
    determines possible species based on user provided location

---
+++ R/get.possible.species.R
@@ -0,0 +1,35 @@
+#' Get List of Possible Species
+#' 
+#' This function will evaluate the user provided latitude / longitude values with range
+#' extent data for species included in the trained models and will return a list of possible species.
+#' 
+#' @param location dataframe containing a single longitude and latitude value
+#' @return
+#' 
+#' @export
+
+
+get.possible.species <- function(location){
+
+  #--Test which species to consider
+  location.test <- vector()
+  
+  for(i in 1:nrow(extent.data)){
+    location.test[i]<-location.contained.in.extent(location, extent.data[i,])
+  }#END Loop
+  
+  possible.species <- extent.data[location.test==TRUE,]
+
+  #--Generate unique set of species 
+  possible.species <- unique(possible.species[,c("taxa","label","model_type")])
+  
+  possible.species <- possible.species[order(possible.species$taxa, possible.species$label),]
+  
+return(possible.species)
+}#END Function
+
+
+
+
+
+
