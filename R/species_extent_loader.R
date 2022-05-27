bc23705b8a7d4b7d55cf1d09341ef1c7fc6872ff
Ryan Miller <ryan.miller@rsmiller.net>
2022-05-27 09:57:47 -0600

    Update species_extent_loader.R

--- R/species_extent_loader.R
+++ R/species_extent_loader.R
@@ -11,8 +11,10 @@ species_extent_loader <- function(){
   data.path <- download_cache(url="https://raw.githubusercontent.com/TabakM/CameraTrapDetectoR/main/Data/species.extent.data.csv")
   
   #--Read Species Extent Data
-  extent.data<-readr::read_csv(data.path, show_col_types=FALSE, progress=FALSE)
-  extent.data<-as.data.frame(extent.data)
+  extent.data<-read.csv(data.path, stringsAsFactors=TRUE)
+
+  #extent.data<-readr::read_csv(data.path, show_col_types=FALSE, progress=FALSE)
+  #extent.data<-as.data.frame(extent.data)
   
   return(extent.data)
 }#END Function
