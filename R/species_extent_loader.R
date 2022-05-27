fefa37d31df6f9c48fbcec623eca379961b3faf5
Ryan Miller <ryan.miller@rsmiller.net>
2022-05-27 09:16:20 -0600

    made reading of data silent

--- R/species_extent_loader.R
+++ R/species_extent_loader.R
@@ -11,8 +11,8 @@ species_extent_loader <- function(){
   data.path <- download_cache(url="https://raw.githubusercontent.com/TabakM/CameraTrapDetectoR/main/Data/species.extent.data.csv")
   
   #--Read Species Extent Data
-  
-  extent.data<-readr::read_csv(data.path)
+  extent.data<-readr::read_csv(data.path, show_col_types=FALSE, progress=FALSE)
+  extent.data<-as.data.frame(extent.data)
   
   return(extent.data)
 }#END Function
